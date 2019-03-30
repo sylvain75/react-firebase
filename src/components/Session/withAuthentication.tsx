580ef6613bf7f90d99620a740786b09a72f992e8
Sylvain <sylvoz75@gmail.com>
2019-03-31 10:16:31 +1100

    auth localstorage usage to remove flicker on reload page

--- src/components/Session/withAuthentication.tsx
+++ src/components/Session/withAuthentication.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import * as firebaseType from 'firebase';
 
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 import Firebase, { AuthUser } from '../Firebase/firebase';
-import * as firebaseType from 'firebase';
+import { AUTH_USER } from '../../constants/localStorageKeys';
 
 type Props = {
   firebase: Firebase
@@ -11,13 +12,17 @@ type Props = {
 
 const withAuthentication = (Component: any) => {
   const withAuthenticationListener = (props: Props) => {
-    const [authUser, setUser] = useState<AuthUser | null>(null);
+    const userFromLocalStorage: any = localStorage.getItem(AUTH_USER);
+    const localAuthUser: AuthUser | null = JSON.parse(userFromLocalStorage);
+    const [authUser, setUser] = useState<AuthUser | null>(localAuthUser);
     useEffect(() => {
       const listener: () => void = props.firebase.onAuthUserListener(
         (authUser: any) => {
+          localStorage.setItem(AUTH_USER, JSON.stringify(authUser));
           setUser(authUser)
         },
         () => {
+          localStorage.removeItem(AUTH_USER);
           setUser(null)
         }
       );
